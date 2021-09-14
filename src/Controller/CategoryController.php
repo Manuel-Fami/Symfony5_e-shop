97ee13836b2c04ce33c43bf6d1d33baea1c17053
Manuel <manuelfamiliar@laposte.net>
2021-09-14 14:38:12 +0200

    Accès isGranted en fonction des Role

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -6,13 +6,19 @@ use App\Entity\Category;
 use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\Repository\RepositoryFactory;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\String\Slugger\SluggerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+
+/**
+ * @IsGranted("ROLE_ADMIN", message= "Vous n'avez pas le droit d'accéder à cette page")
+ *
+ */
 class CategoryController extends AbstractController
 {
     /**
