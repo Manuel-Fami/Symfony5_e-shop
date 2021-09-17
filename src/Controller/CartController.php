7e03098e02a36ce016e7f25cac532cbf256453f3
Manuel <manuelfamiliar@laposte.net>
2021-09-17 15:50:44 +0200

    ControllCart mise en place du panier '/cart' + Template cart/index

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,12 +2,6 @@
 
 namespace App\Controller;
 
-<<<<<<< HEAD
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-=======
 use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,21 +10,10 @@ use Symfony\Component\HttpFoundation\Session\Flash\FlashBag;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
->>>>>>> second
 
 class CartController extends AbstractController
 {
     /**
-<<<<<<< HEAD
-     * @Route("/cart/add/{id}", name="cart_add")
-     */
-    public function add($id, Request $request)
-    {
-
-        //1. Retrouver le panier dans la session (sous forme de tableau)
-        //2. Si il n'existe pas encore, alors prendre un tableau vide
-        $cart = $request->getSession()->get('cart', []);
-=======
      * @Route("/cart/add/{id}", name="cart_add", requirements ={"id":"\d+"})
      */
     public function add($id, ProductRepository $productRepository, SessionInterface $session, FlashBagInterface $flashBag)
@@ -45,7 +28,6 @@ class CartController extends AbstractController
         //1. Retrouver le panier dans la session (sous forme de tableau)
         //2. Si il n'existe pas encore, alors prendre un tableau vide
         $cart = $session->get('cart', []);
->>>>>>> second
 
         //3. Voir si le produit ($id) existe déjà dans le tableau
         //4. Si c'est le cas, simplement augmenter la quantité
@@ -57,9 +39,6 @@ class CartController extends AbstractController
         }
 
         //6. Enregistrer le tableau mis à jour dans la session
-<<<<<<< HEAD
-        $request->getSession()->set('cart', $cart);
-=======
         $session->set('cart', $cart);
         // dd($session);
 
@@ -67,18 +46,38 @@ class CartController extends AbstractController
 
         // dump($flashBag->get('success'));
         // dd($flashBag);
->>>>>>> second
 
         //Permet de supprimer la session - remise à zéro
         // $request->getSession()->remove('cart');
 
-<<<<<<< HEAD
-        dd($request->getSession()->get('cart'));
-=======
         return $this->redirectToRoute('product_show', [
             'category_slug' => $product->getCategory()->getSlug(),
             'slug' => $product->getSlug()
         ]);
->>>>>>> second
+    }
+
+    /**
+     * @Route("/cart", name="cart_show")
+     */
+    public function show(SessionInterface $session, ProductRepository $productRepository)
+    {
+        $detailedCart = [];
+        $total = 0;
+
+        foreach ($session->get('cart', []) as $id => $qty) {
+            $product = $productRepository->find($id);
+
+            $detailedCart[] = [
+                'product' => $product,
+                'qty' => $qty
+            ];
+
+            $total += ($product->getPrice() * $qty);
+        }
+
+        return $this->render('cart/index.html.twig', [
+            'items' => $detailedCart,
+            'total' => $total
+        ]);
     }
 }
