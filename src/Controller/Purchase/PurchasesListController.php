528d85b899065c2e8213d477e462790aa4b14b4c
Manuel <manuelfamiliar@laposte.net>
2021-09-23 12:36:06 +0200

    Liste commande terminé

--- src/Controller/Purchase/PurchasesListController.php
+++ src/Controller/Purchase/PurchasesListController.php
@@ -6,8 +6,10 @@ use Twig\Environment;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class PurchasesListController extends AbstractController
 {
@@ -32,8 +34,7 @@ class PurchasesListController extends AbstractController
 
         //Vérification connexion user + création d'une url pour rediriger sur la page principale
         if (!$user) {
-            $url = $this->router->generate('homepage');
-            return new RedirectResponse($url);
+            throw new AccessDeniedHttpException("Vous devez être connecté pour accéder à vos commandes !");
         }
 
         // Qui est connecté ?
