0602a6ead5cf490b5be44b479c70b7eb359005a0
Manuel <manuelfamiliar@laposte.net>
2021-09-23 11:41:55 +0200

    Function listpurchase

--- src/Controller/Purchase/PurchasesListController.php
+++ src/Controller/Purchase/PurchasesListController.php
@@ -2,12 +2,36 @@
 
 namespace App\Controller\Purchase;
 
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Routing\RouterInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class PurchasesListController extends AbstractController
 {
+    protected $security;
+    protected $router;
 
+    public function __construct(Security $security, RouterInterface $router)
+    {
+        $this->security = $security;
+        $this->router = $router;
+    }
+    /**
+     * @Route("/purchases", name="purchase_index")
+     */
     public function index()
     {
+        // Connection ok ?
+        $user = $this->security->getUser();
+
+        //Vérification connexion user + création d'une url pour rediriger sur la page principale
+        if (!$user) {
+            $url = $this->router->generate('homepage');
+            return new RedirectResponse($url);
+        }
+
+        // Qui est connecté ?
+
     }
 }
