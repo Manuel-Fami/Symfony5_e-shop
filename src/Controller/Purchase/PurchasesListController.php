ec18878e185a5c9fc93bcca82b4981a534763a95
Manuel <manuelfamiliar@laposte.net>
2021-09-23 11:32:33 +0200

    Création dossier Purchase dans Controller

---
+++ src/Controller/Purchase/PurchasesListController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller\Purchase;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class PurchasesListController extends AbstractController
+{
+
+    public function index()
+    {
+    }
+}
